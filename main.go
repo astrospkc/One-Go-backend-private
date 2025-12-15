ebd1e52a3c9b95056a5a80d3b3d1e30ea2f266cd
Punam <punamkumari399@gmail.com>
2025-12-16 00:20:39 +0530

    getting the result by testing gemini

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"gobackend/config"
 	"gobackend/connect"
 	"gobackend/routes"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"google.golang.org/genai"
 )
 
 // https://one-go-private.vercel.app
@@ -44,5 +48,35 @@ func main() {
 	routes.RegisterNormalRoutes(app)
 	routes.RegisterAPIKeyRoutes(app)
 
+	// ---------------------------------------------
+	// Gemini testing
+	chat, err := connect.GeminiClient.Chats.Create(context.Background(), "gemini-2.5-flash", nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := chat.SendMessage(context.Background(), genai.Part{Text: "What's the weather in New York?"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	debugPrint(result)
+
+	result, err = chat.SendMessage(context.Background(), genai.Part{Text: "How about San Francisco?"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	debugPrint(result)
+	// ------------------------------------------
+
 	app.Listen(":8080")
+}
+
+func debugPrint[T any](r *T) {
+
+	response, err := json.MarshalIndent(*r, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(response))
 }
